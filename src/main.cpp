39ab19317c7be5240825abd5ce7f3d34b37c76b5
joshua-9402 <145089285+joshua-9402@users.noreply.github.com>
2025-10-31 14:12:19 +0800

    major changes in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,35 +1,8 @@
-#include "hello_imgui/hello_imgui.h"
-#include "imgui.h"
+#include <string>
+#include "lib/UI.h"
 
-void MainGUI()
-{
-    ImGui::Text("Hello, BCpET 1101!");
-    if (ImGui::Button("Exit"))
-        HelloImGui::GetRunnerParams()->appShallExit = true;
-}
+int main() {
+    constructUI("CpET 140 - Final Project", "fonts/OpenSans-Regular.ttf",1600, 900, "main");
 
-void POS() {}
-
-void Inventory() {}
-
-
-int main()
-{
-    HelloImGui::RunnerParams params;
-
-    // Load your custom font from assets/fonts
-    params.callbacks.LoadAdditionalFonts = []()
-    {
-        const ImGuiIO& io = ImGui::GetIO();
-        const std::string fontPath = HelloImGui::AssetFileFullPath("fonts/OpenSans-Regular.ttf");
-        io.Fonts->AddFontFromFileTTF(fontPath.c_str(), 22.0f);
-    };
-
-    // Window and GUI settings
-    params.callbacks.ShowGui = MainGUI;
-    params.appWindowParams.windowTitle = "CpET 140 - Final Project";
-    params.appWindowParams.windowGeometry.size = {1600, 900};
-
-    HelloImGui::Run(params);
     return 0;
 }
