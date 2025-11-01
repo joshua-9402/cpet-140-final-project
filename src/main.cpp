4060dd0870617f8c23fec84d4bc968bd3ab548d0
joshua-9402 <145089285+joshua-9402@users.noreply.github.com>
2025-11-01 09:17:22 +0800

    refactor main function to use constants for UI parameters

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,13 @@
 #include <string>
 #include "lib/UI.h"
 
+const std::string appTitle = "Main Window";
+const std::string fontName = "fonts/OpenSans-Regular.ttf";
+constexpr int windowWidth = 1600;
+constexpr int windowHeight = 900;
+
 int main() {
-    constructUI("CpET 140 - Final Project", "fonts/OpenSans-Regular.ttf",1600, 900, "main");
+    constructUI(appTitle, fontName,windowWidth, windowHeight, "test");
 
     return 0;
 }
