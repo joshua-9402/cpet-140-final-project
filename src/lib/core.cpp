6e3e783eb2f6404022a367e24d8413720089793d
joshua-9402 <145089285+joshua-9402@users.noreply.github.com>
2025-11-01 09:17:09 +0800

    add core module with version retrieval functionality

---
+++ src/lib/core.cpp
@@ -0,0 +1,4 @@
+#include "core.h"
+#include <string>
+
+std::string getCoreVersion() {return "1.0.0";}
---
+++ src/lib/core.h
@@ -0,0 +1,13 @@
+//
+// Created by Josh Literal on 10/31/25.
+//
+
+#ifndef CPET_140_FINAL_PROJECT_CORE_H
+#define CPET_140_FINAL_PROJECT_CORE_H
+
+
+class core {
+};
+
+
+#endif //CPET_140_FINAL_PROJECT_CORE_H
