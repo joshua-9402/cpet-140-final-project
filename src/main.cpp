fef8e7cded8f598dd4d08ce817a16cecfabfccbd
joshua-9402 <145089285+joshua-9402@users.noreply.github.com>
2025-11-09 20:55:23 +0800

    refactor: enhance main.cpp with authentication and I/O handling functions

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,7 @@
  *
  * Purpose
  * - Starts the application, do any necessary checks, constructs the UI and finally run the application.
+ * - Handles auth, incoming, and outgoing data from / to the system
  *
  * Boundaries
  * - Entry point only; no business logic or persistence here.
@@ -24,7 +25,29 @@ constexpr int g_windowWidth = 1600;
 constexpr int g_windowHeight = 900;
 
 
+/*
+ * This function is responsible for the connection to and from the mobile application
+ *
+ * For more information, please refer to the mobile application's repository
+ */
+std::string authGateway(const std::string& username, const std::string& password, const std::string& verificationCode) {
+    if (verificationCode.empty()) {
+        ui::g_failedMessage = "error: invalid verification code";
+        ui::constructUI(g_appTitle, g_fontName, g_windowWidth, g_windowHeight, "failedUI");
+    }
+    return username + "_" + password + "_" + verificationCode;
+}
+
+
+/*
+ *
+ */
+std::string ioHandler(const std::string& io, const std::string& command) {
+    return "val " +  io + " " + command;
+}
+
+
 int main() {
-    constructUI(g_appTitle, g_fontName, g_windowWidth, g_windowHeight, "main");
+    ui::constructUI(g_appTitle, g_fontName, g_windowWidth, g_windowHeight, "main");
     return 0;
 }
