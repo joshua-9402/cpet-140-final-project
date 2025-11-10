59c1e9daa9b837e79d0cb8b78da352bd7e4b3d02
joshua-9402 <145089285+joshua-9402@users.noreply.github.com>
2025-11-10 12:26:18 +0800

    refactor: add io.cpp and io.h files for input/output functionality

---
+++ src/handler/io.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Josh Literal on 11/10/25.
+//
+
+#include "io.h"
---
+++ src/handler/io.h
@@ -0,0 +1,13 @@
+//
+// Created by Josh Literal on 11/10/25.
+//
+
+#ifndef CPET_140_FINAL_PROJECT_IO_H
+#define CPET_140_FINAL_PROJECT_IO_H
+
+
+class io {
+};
+
+
+#endif //CPET_140_FINAL_PROJECT_IO_H
