388fac7cc5dc5e205ee4562c0a5ce161136f26d6
joshua-9402 <145089285+joshua-9402@users.noreply.github.com>
2025-11-15 00:11:17 +0800

    refactor: update authentication UI to use g_loginTitle constant for consistency

--- src/main.cpp
+++ src/main.cpp
@@ -25,7 +25,7 @@ int main() {
     while (true) {
         const config appConfig;
         if (!g_auth) {
-            ui::constructUI(appConfig.g_appTitle, appConfig.g_fontName, appConfig.g_smallWidth, appConfig.g_smallHeight, "auth"); // Show authentication UI
+            ui::constructUI(appConfig.g_loginTitle, appConfig.g_fontName, appConfig.g_smallWidth, appConfig.g_smallHeight, "auth"); // Show authentication UI
             if (!g_auth) {break;} // If user didn't log in (closed the window), exit the application
         }
         ui::constructUI(appConfig.g_appTitle, appConfig.g_fontName, appConfig.g_defaultWidth, appConfig.g_defaultHeight, "main"); // Show main UI
