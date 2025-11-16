5b73603f5fc55c7e436de7393b9c5cafc1572047
joshua-9402 <145089285+joshua-9402@users.noreply.github.com>
2025-11-16 23:29:43 +0800

    add: implement system checks for libsodium initialization and necessary directory/file creation

--- src/main.cpp
+++ src/main.cpp
@@ -12,6 +12,8 @@
  * - Entry point only; no business logic or persistence here.
  *
  * Notes
+ * - Check for any dependencies or required files/directories before starting.
+ * - Loop to show login UI if not authenticated, then main UI if authenticated.
  * - Configures title, font, and window size, then calls constructUI().
  */
 
@@ -19,16 +21,37 @@
 #include <string>
 #include "ui/ui.h"
 #include "config/app_config.h"
+#include "handler/auth.h"
+#include "handler/system.h"
+#include "handler/db.h"
+
+
+void systemCheck() {
+    // System check: Ensure libsodium is initialized properly
+    if (!auth::checkSodium()) {
+        ui::g_failedMessage = "Error: libsodium initialization failed.";
+        ui::constructUI(appConfig::g_errorTitle, appConfig::g_fontName, appConfig::g_errorWidth, appConfig::g_defaultHeight, "failed");
+    }
+
+    // Create necessary directories and files
+    if (!system::searchDirectory("data")) {system::createDirectory("data");}
+    if (!system::searchDirectory("projects")) {system::createDirectory("projects");}
+    if (!db::searchFileText("data/users.txt")) {db::createFileText("data/users.txt");}
+    if (db::searchDatabase(appConfig::g_dbNamePayroll) == false) {db::createDatabase(appConfig::g_dbNamePayroll);}
+    if (db::searchDatabase(appConfig::g_dbNameTracker) == false) {db::createDatabase(appConfig::g_dbNameTracker);}
+}
 
 int main() {
-    // Main application loop: show login/main UI based on authentication status
+    // Perform system checks before starting the application
+    systemCheck();
+
+    // Main application loop
     while (true) {
-        const config appConfig;
         if (!g_auth) {
-            ui::constructUI(appConfig.g_loginTitle, appConfig.g_fontName, appConfig.g_smallWidth, appConfig.g_smallHeight, "auth"); // Show authentication UI
+            ui::constructUI(appConfig::g_loginTitle, appConfig::g_fontName, appConfig::g_loginWidth, appConfig::g_loginHeight, "auth"); // Show authentication UI
             if (!g_auth) {break;} // If user didn't log in (closed the window), exit the application
         }
-        ui::constructUI(appConfig.g_appTitle, appConfig.g_fontName, appConfig.g_defaultWidth, appConfig.g_defaultHeight, "main"); // Show main UI
+        ui::constructUI(appConfig::g_appTitle, appConfig::g_fontName, appConfig::g_defaultWidth, appConfig::g_defaultHeight, "main"); // Show main UI
     }
 
     return 0;
