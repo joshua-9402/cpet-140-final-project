b64675c3e92464b902ebeddbc37906c184bb628e
joshua-9402 <145089285+joshua-9402@users.noreply.github.com>
2025-11-17 21:59:45 +0800

    add: create master key setup guide and secure store module documentation

---
+++ src/security/storage.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Josh Literal on 11/17/25.
+//
+
+#include "storage.h"
---
+++ src/security/storage.h
@@ -0,0 +1,13 @@
+//
+// Created by Josh Literal on 11/17/25.
+//
+
+#ifndef CPET_140_FINAL_PROJECT_STORAGE_H
+#define CPET_140_FINAL_PROJECT_STORAGE_H
+
+
+class storage {
+};
+
+
+#endif //CPET_140_FINAL_PROJECT_STORAGE_H
