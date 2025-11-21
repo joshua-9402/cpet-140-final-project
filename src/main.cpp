1c01498ff5d0e1841bccffff516926a79fd99832
joshua-9402 <145089285+joshua-9402@users.noreply.github.com>
2025-11-21 20:07:06 +0800

    feat: enhance system and authentication checks with SQLite availability and directory management

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 /*
  * CpET 140 Final Project — Application entry point
+ * StructuraCost - Main application entry point
  *
  * Contributors:
  *  Joshua Literal
@@ -19,37 +20,53 @@
 
 
 #include <string>
+
 #include "ui/ui.h"
-#include "config/app_config.h"
-#include "handler/auth.h"
+#include "config/config.h"
+#include "security/security.h"
 #include "handler/system.h"
 #include "handler/db.h"
+#include "security/cryptography.h"
 
 
 void systemCheck() {
     // System check: Ensure libsodium is initialized properly
-    if (!auth::checkSodium()) {
+    if (!cryptography::checkSodium()) {
         ui::g_failedMessage = "Error: libsodium initialization failed.";
         ui::constructUI(appConfig::g_errorTitle, appConfig::g_fontName, appConfig::g_errorWidth, appConfig::g_defaultHeight, "failed");
     }
 
+    // Database check: Ensure SQLite is available
+    if (!db::isSQLiteAvailable()) {
+        ui::g_failedMessage = "Error: sqlite initialization failed.";
+        ui::constructUI(appConfig::g_errorTitle, appConfig::g_fontName, appConfig::g_errorWidth, appConfig::g_defaultHeight, "failed");
+    }
+
     // Create necessary directories and files
-    if (!system::searchDirectory("data")) {system::createDirectory("data");}
-    if (!system::searchDirectory("projects")) {system::createDirectory("projects");}
-    if (!db::searchFileText("data/users.txt")) {db::createFileText("data/users.txt");}
-    if (db::searchDatabase(appConfig::g_dbNamePayroll) == false) {db::createDatabase(appConfig::g_dbNamePayroll);}
-    if (db::searchDatabase(appConfig::g_dbNameTracker) == false) {db::createDatabase(appConfig::g_dbNameTracker);}
+    if (!system::searchFile("test.txt")) {system::createFile("test.txt");}
+    if (!system::searchDirectory("logs")) {system::createDirectory("logs");}
+    if (!system::searchDirectory(appConfig::g_dataDirectory)) {system::createDirectory(appConfig::g_dataDirectory);}
+    if (!system::searchDirectory(appConfig::g_dataDirectory + appConfig::g_projectDirectory)) {system::createDirectory(appConfig::g_dataDirectory + appConfig::g_projectDirectory);}
+
+    if (db::searchDatabase(appConfig::g_dataDirectory + appConfig::g_dbNamePayroll) == false) {db::createDatabase(appConfig::g_dataDirectory + appConfig::g_dbNamePayroll);}
+    if (db::searchDatabase(appConfig::g_dataDirectory + appConfig::g_dbNameTracker) == false) {db::createDatabase(appConfig::g_dataDirectory + appConfig::g_dbNameTracker);}
+}
+
+
+void authenticationCheck() {
+
 }
 
 int main() {
-    // Perform system checks before starting the application
+    // Perform checks before starting the application
     systemCheck();
+    authenticationCheck();
 
     // Main application loop
     while (true) {
-        if (!g_auth) {
+        if (!appConfig::g_auth) {
             ui::constructUI(appConfig::g_loginTitle, appConfig::g_fontName, appConfig::g_loginWidth, appConfig::g_loginHeight, "auth"); // Show authentication UI
-            if (!g_auth) {break;} // If user didn't log in (closed the window), exit the application
+            if (!appConfig::g_auth) {break;} // If user didn't log in (closed the window), exit the application
         }
         ui::constructUI(appConfig::g_appTitle, appConfig::g_fontName, appConfig::g_defaultWidth, appConfig::g_defaultHeight, "main"); // Show main UI
     }
