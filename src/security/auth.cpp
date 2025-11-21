3b1a1004ff58fdf88155d8478be9332e1e845d48
joshua-9402 <145089285+joshua-9402@users.noreply.github.com>
2025-11-21 20:06:00 +0800

    feat: implement authentication module with user credential verification methods

--- src/security/auth.cpp
+++ src/security/auth.cpp
@@ -1,2 +1,49 @@
+/*
+* CpET 140 Final Project — Authentication module
+ * StructuraCost - Security - Authentication module
+ *
+ * Contributors:
+ *  Joshua Literal
+ *
+ * Purpose
+ * - Handle authentication for test and admin users.
+ * - Provides methods to verify credentials.
+ *
+ * Boundaries
+ * - Interacts with the security module for credential verification.
+ *
+ * Notes
+ * - This module is crucial for ensuring only authorized access to the application.
+ * - In a real application, credentials would be securely stored and managed.
+ */
+
+
+#include <string>
+
 #include "auth.h"
 
+#include "../security/cryptography.h"
+
+
+bool auth::testAuth(const std::string& username, const std::string& password) {
+    if ( username == "testUsername" || password == "testPassword") {
+        return true;
+    }
+    return false;
+}
+
+
+bool auth::testDeployAuth(const std::string& username, const std::string& password) {
+    if ( username == "name" && password == "pass") {
+        return true;
+    }
+    return false;
+}
+
+
+bool auth::adminAuth(const std::string& username, const std::string& password) {
+    if (username == "admin" && cryptography::saltKey(cryptography::hashKey(password)) == "76rtdychgnih") {
+        return true;
+    }
+    return false;
+}
--- src/security/auth.h
+++ src/security/auth.h
@@ -1,8 +1,16 @@
 #ifndef CPET_140_FINAL_PROJECT_AUTH_H
 #define CPET_140_FINAL_PROJECT_AUTH_H
 
+#include <string>
+
 
 class auth {
+    public:
+        static bool testAuth(const std::string& username, const std::string& password);
+
+        static bool testDeployAuth(const std::string &username, const std::string &password);
+
+        static bool adminAuth(const std::string& username, const std::string& password);
 };
 
 
