54a177991452f907e22274d5bb968bf42317a656
joshua-9402 <145089285+joshua-9402@users.noreply.github.com>
2025-12-21 22:44:28 +0800

    feat: add new UI methods and include hello_imgui for enhanced rendering capabilities

--- src/ui/ui.h
+++ src/ui/ui.h
@@ -21,6 +21,7 @@
 #define CPET_140_FINAL_PROJECT_UI_H
 
 #include <string>
+#include "hello_imgui/hello_imgui.h"
 
 class ui {
     public:
@@ -31,6 +32,13 @@ class ui {
 
         // Global Methods
         static void constructUI(const std::string &a_title, const std::string& a_fontLocation, int a_widthPx, int a_heightPx, const std::string& a_window);
-
+        static void renderStatCard(const char* childId, const char* title, const ImVec4& titleColor, const std::string& valueText, const char* subtitle, const ImVec2& size);
+        static std::string normalizeProjectId(const std::string& input);
+        static std::string buildProjectReportOutFile(const std::string& projectID);
+        static ImVec2 fullWidthButtonSize(float a_height);
+        static void setButtonCenter(const char *text, const ImVec2 &size);
+        static void setTextCenter(const char *text);
+        static void setTextRight(const char *text);
+        static void loadImage(const std::string &p_location, float p_locationXPx, float p_locationYPx, float p_imageSize);
 };
 #endif //CPET_140_FINAL_PROJECT_UI_H
